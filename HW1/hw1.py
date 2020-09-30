7695ab3a176e1f3fde3ef7cfc60b0b110c9f257c
yanbronshtein <yanbronshtein@gmail.com>
2020-09-30 14:48:17 -0400

    Created Node class

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -3,71 +3,44 @@ import pandas as pd
 import sys
 from queue import PriorityQueue
 import itertools
+
 # x and y coordinates with top left corner as origin
 goal_grid = {
-    1:(0,0), 2:(1,0), 3: (2,0),
-    8:(0,1), 0:(1,1), 4: (2,1),
-    7:(0,2), 6:(1,2), 5: (2,2)
+    1: (0, 0), 2: (1, 0), 3: (2, 0),
+    8: (0, 1), 0: (1, 1), 4: (2, 1),
+    7: (0, 2), 6: (1, 2), 5: (2, 2)
 }
 
-#Out of place 4
+# Out of place 4
 easy_grid = {
-    1:(0,0), 3:(1,0), 4: (2,0),
-    8:(0,1), 6:(1,1), 2: (2,1),
-    7:(0,2), 0:(1,2), 5: (2,2)
+    1: (0, 0), 3: (1, 0), 4: (2, 0),
+    8: (0, 1), 6: (1, 1), 2: (2, 1),
+    7: (0, 2), 0: (1, 2), 5: (2, 2)
 }
-#Out of place 5
+# Out of place 5
 medium_grid = {
-    2:(0,0), 8:(1,0), 1: (2,0),
-    0:(0,1), 4:(1,1), 3: (2,1),
-    7:(0,2), 6:(1,2), 5: (2,2)
+    2: (0, 0), 8: (1, 0), 1: (2, 0),
+    0: (0, 1), 4: (1, 1), 3: (2, 1),
+    7: (0, 2), 6: (1, 2), 5: (2, 2)
 }
 
-#Out of place 7
+# Out of place 7
 hard_grid = {
-    2:(0,0), 8:(1,0), 1: (2,0),
-    4:(0,1), 6:(1,1), 3: (2,1),
-    0:(0,2), 7:(1,2), 5: (2,2)
+    2: (0, 0), 8: (1, 0), 1: (2, 0),
+    4: (0, 1), 6: (1, 1), 3: (2, 1),
+    0: (0, 2), 7: (1, 2), 5: (2, 2)
 }
 
-#out of place
+# out of place
 worst_grid = {
-    5:(0,0), 6:(1,0), 7: (2,0),
-    4:(0,1), 0:(1,1), 8: (2,1),
-    3:(0,2), 2:(1,2), 1: (2,2)
+    5: (0, 0), 6: (1, 0), 7: (2, 0),
+    4: (0, 1), 0: (1, 1), 8: (2, 1),
+    3: (0, 2), 2: (1, 2), 1: (2, 2)
 }
 
-#use easy and goal
-def calc_out_of_place_tiles(start_grid):
-    # for coordinates in easy_grid.values():
-    #     print(coordinates)
-    num_out_of_place_tiles = 0
-    for (k1, v1), (k2,v2) in zip(start_grid.items(), goal_grid.items()):
-        #We don't care if the blank is out of place
-        if k1 != k2 and k1!= 0 :
-            num_out_of_place_tiles += 1
-        print("start:" + str(k1), str(v1))
-        print("goal:" + str(k2), str(v2))
-
-    return num_out_of_place_tiles
+# use easy and goal
 
 
-# calc_out_of_place_tiles(worst_grid)
-
-
-def calc_sum_manhattan_distance(start_grid):
-    # for coordinates in easy_grid.values():
-    #     print(coordinates)
-    total_manhattan_distance = 0
-    for num in start_grid:
-        #     #We don't care if the blank is out of place
-        if num != 0:
-            total_manhattan_distance += (abs(start_grid[num][0] - goal_grid[num][0]) + (abs(start_grid[num][1] - goal_grid[num][1])))
-
-    return total_manhattan_distance
-
-
-calc_sum_manhattan_distance(easy_grid)
 '''
 def state:
     matrix
@@ -80,32 +53,42 @@ def state:
 '''
 
 
+class Node:
+    def __init__(self, parent_node, use_manhattan, curr_grid):
+        self.parent_node = parent_node
+        self.g = parent_node.g + 1
+        self.curr_grid = curr_grid
+        self.h = self.calc_manhattan_heuristic(
+            self.curr_grid) if use_manhattan else self.calc_misplaced_tiles_heuristic(self.curr_grid)
+        self.f = self.g + self.h
 
-# def calculateManhattan(initial_state):
-#     initial_config = initial_state
-#     manDict = 0
-#     for i,item in enumerate(initial_config):
-#         prev_row,prev_col = int(i/ 3) , i % 3
-#         goal_row,goal_col = int(item /3),item % 3
-#         manDict += abs(prev_row-goal_row) + abs(prev_col - goal_col)
-#     return manDict
-#
-#
-#
-# class state:
-#     def __init__(self,matrix, parent_node):
-#
-# def h_manhattan(node1,node2):
-#
-# def h(node):
-#     pass
-#
-# def f(node):
-#     # returns number
-#     pass
-#
-# def g(node):
-#     pass
+    def calc_misplaced_tiles_heuristic(self, curr_grid):
+        # for coordinates in easy_grid.values():
+        #     print(coordinates)
+        misplaced_tiles = 0
+        for (k1, v1), (k2, v2) in zip(curr_grid.items(), goal_grid.items()):
+            # We don't care if the blank is out of place
+            if k1 != k2 and k1 != 0:
+                misplaced_tiles += 1
+            print("start:" + str(k1), str(v1))
+            print("goal:" + str(k2), str(v2))
+
+        return misplaced_tiles
+
+    # calc_out_of_place_tiles(worst_grid)
+
+    def calc_manhattan_heuristic(self, curr_grid):
+        # for coordinates in easy_grid.values():
+        #     print(coordinates)
+        total_manhattan_distance = 0
+        for num in curr_grid:
+            #     #We don't care if the blank is out of place
+            if num != 0:
+                total_manhattan_distance += (
+                        abs(curr_grid[num][0] - goal_grid[num][0]) + (abs(curr_grid[num][1] - goal_grid[num][1])))
+
+        return total_manhattan_distance
+
 #
 # def a_star_search(start_state, goal_state, heuristic='not_manhattan'):
 #     open_list = PriorityQueue()
