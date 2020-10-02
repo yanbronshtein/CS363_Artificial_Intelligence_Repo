0ea5648714dfa7d1abffb1bdee88e7e68c58f49e
yanbronshtein <yanbronshtein@gmail.com>
2020-10-02 00:24:35 -0400

    Did documentation for everything except for a * search

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -27,10 +27,19 @@ hard_grid = {
 
 
 class Node:
+
     def __init__(self, curr_grid: dict, use_manhattan: bool, parent_node=None):
+        """
+        Constructor for state in 8 puzzle
+        :param curr_grid: 3x3 dictionary with key the number and value the tuple for coordinates in the grid
+        :param use_manhattan: if set to true calculates h using manhattan heuristic. Otherwise use misplaced tiles
+        :param parent_node: parent of the current node
+        """
         self.use_manhattan = use_manhattan
         self.curr_grid = curr_grid
         self.blank_pos = self.curr_grid[blank]  # Get Position of the blank
+
+        # If the current node has a parent, calculate g,h, and f. Else, initialize f,g, and h to 0
         if parent_node is not None:
             self.parent_node = parent_node
             self.g = parent_node.g + 1
@@ -41,22 +50,33 @@ class Node:
             self.h = 0
             self.f = 0
 
-    def calc_misplaced_tiles_heuristic(self):
-        # for coordinates in easy_grid.values():
-        #     print(coordinates)
+    def calc_misplaced_tiles_heuristic(self) -> int:
+        """
+        This method uses the misplaced tile heuristic that compares the position of each number in the current state
+        grid to that in the goal state grid
+        :return: total number of misplaced tiles
+        """
+
+        # Iterate through each grid represented by a dictionary.
+        # We only care about checking the position of the non-blank numbers.
         misplaced_tiles = 0
         for (k1, v1), (k2, v2) in zip(self.curr_grid.items(), goal_grid.items()):
             # We don't care if the blank is out of place
-            if k1 != k2 and k1 != 0:
+            if k1 != k2 and k1 != blank:
                 misplaced_tiles += 1
 
         return misplaced_tiles
 
-    def calc_manhattan_heuristic(self):
-        # for coordinates in easy_grid.values():
+    def calc_manhattan_heuristic(self) -> int:
+        """
+        This method uses the manhattan heuristic that calculates the sum of all the manhattan distances
+        between the tiles in the current grid and the goal grid.
+        The manhattan distance is defined as the  distance between two points measured along axes at right angles
+         see: https://xlinux.nist.gov/dads/HTML/manhattanDistance.html
+        :return: sum total of manhattan distances
+        """
         total_manhattan_distance = 0
         for num in self.curr_grid:
-            #     #We don't care if the blank is out of place
             if num != blank:
                 total_manhattan_distance += (
                         abs(self.curr_grid[num][blank] - goal_grid[num][blank]) + (abs(self.curr_grid[num][1] -
@@ -64,9 +84,14 @@ class Node:
 
         return total_manhattan_distance
 
-    def generate_successors(self):
-        row, col = self.blank_pos
+    def generate_successors(self) -> list:
+        """
+        This method generates all possible moves for the blank tile
+        :return: A list of Node objects representing the resulting grids after the available moves are applied
+        """
 
+        row, col = self.blank_pos
+        # Definition of moves available.
         right = (row, col + 1)
         down = (row + 1, col)
         left = (row, col - 1)
@@ -74,29 +99,45 @@ class Node:
 
         potential_moves = [right, down, left, up]
 
+        # Remove moves that result in the tile moving out of the bounds of the grid
         for move in potential_moves:
             row, col = move
             if row > 2 or col > 2 or row < 0 or col < 0:
                 potential_moves.remove(move)
-        successors = []
 
+        # Generate all the valid grid configurations after the valid moves are applied and append them to
+        # the successor list
+        successors = []
         for move in potential_moves:
             successors.append(self.create_successor(move))
 
         return successors
 
     def create_successor(self, new_blank_pos: tuple):
-        replace_key = -1
+        """
+        This method creates a new node containing the grid configuration reflecting the blank tile move.
+        This involves swapping the blank tile with the adjacent tile
+        :param new_blank_pos: tuple containing the new coordinates of the blank tile
+        :return: Node containing the new grid configuration
+        """
+        # Determine the number currently located in the desired position of the blank tile
+        tile_to_replace = -1
         new_grid = self.curr_grid.copy()
         for key in new_grid:
             if new_grid[key] == new_blank_pos:
-                replace_key = key
+                tile_to_replace = key
                 break
-        new_grid[blank] = new_blank_pos  # Set the new position of the blank to be the new position
-        new_grid[replace_key] = self.blank_pos  # Set the position of the replace_key to the old position of the blank
+
+        # Swap the positions of the blank and the replaced tile
+        new_grid[blank] = new_blank_pos
+        new_grid[tile_to_replace] = self.blank_pos  # Set the position of the replaced number to the old position of the blank
         return Node(new_grid, self.use_manhattan, self)
 
     def __repr__(self):
+        """
+        Generates the string representation of the Node object grid for printing purposes
+        :return: string representation
+        """
         arr = np.empty([3, 3], np.int)
         for num in self.curr_grid:
             row, col = self.curr_grid[num]
@@ -112,93 +153,109 @@ class Node:
         return return_str
 
 
-# class for Priority queue
+
 class PriorityQueue:
 
     def __init__(self):
-        self.queue = list()
-        # if you want you can set a maximum size for the queue
+        """
+        This constructor creates an empty list for the Priority Queue
+        """
+        self.p_queue = []
 
-    def insert(self, node: Node):
-        # if queue is empty
+    def insert(self, node: Node) -> bool:
+        """
+        This method inserts a node into the priority queue.
+        If the internal list is empty, the new node is appended.
+        Otherwise, find the right place for node depending on the value of f determined either by the manhattan or
+        misplaced tile heuristics
+        :param node: Node to be inserted
+        :return: whether the node was successfully inserted
+        """
         if self.size() == 0:
-            # add the new node
-            self.queue.append(node)
+            self.p_queue.append(node)
         else:
-            # traverse the queue to find the right place for new node
-            for x in range(self.size()):
-                # if the priority of new node is greater
-                if node.f >= self.queue[x].f:
-                    # if we have traversed the complete queue
-                    if x == (self.size() - 1):
-                        # add new node at the end
-                        self.queue.insert(x + 1, node)
+            # Iterate through entire list.
+            # If the f value is at least that of the current node:
+            #   If we are at the end, append the node. Else keep iterating
+            # Else insert the node right before the current node
+            for i in range(self.size()):
+
+                if node.f >= self.p_queue[i].f:
+                    if i == (self.size() - 1):
+                        self.p_queue.insert(i + 1, node)
                     else:
                         continue
                 else:
-                    self.queue.insert(x, node)
+                    self.p_queue.insert(i, node)
                     return True
 
     def delete(self) -> Node:
-        # remove the first node from the queue
-        return self.queue.pop(0)
+        """
+        This method removes the first node in the queue
+        :return: removed element
+        """
+        return self.p_queue.pop(0)
 
     def size(self):
-        return len(self.queue)
+        """
+        :return: The length of the internal list
+        """
+        return len(self.p_queue)
 
 
 def a_star_search(start_grid, use_manhattan=False):
-    # Initialize the open list
+    print("hi")
+    ''' 
+    Create open list of nodes, initially containing only starting node
+    '''
     open_list = PriorityQueue()
     first_node = Node(easy_grid, True)
     open_list.insert(first_node)
 
-    # Initialize the closed list
+    '''
+    Create the closed list of nodes, initially empty
+    '''
     closed_list = []
+
+    while True:
+        '''
+        Consider the best node in the open list (the node with the lowest f value)
+        '''
+        best_node = open_list.delete()
+
+        if best_node.curr_grid == goal_grid:
+            print("The goal has been reached")
+            break
+        else:
+            closed_list.append(best_node)
+            successors = best_node.generate_successors()
+            for successor in successors:
+                if successor in closed_list and best_node.g < successor.g:
+                    '''
+                    move the node from the closed list to the open list
+                    '''
+                    open_list.insert(successor)
+                    closed_list.remove(successor)
+
+                    '''update the neighbor with the new, lower g value'''
+                    successor.g = best_node.g
+
+                    '''change the neighbor's parent to our current node'''
+                    successor.parent_node = best_node
+
+    # Initialize the closed list
 
     q = open_list.delete()
     print(q)
     # print(type(q))
     successors = q.generate_successors()
 
-    print("Hi")
-    # while open_list:
-    #     '''
-    #     Find the node with the least f. Call it "q" Pop it off the list. I am using a priority queue so this is done
-    #     automatically
-    #     '''
-    #     q = open_list.delete()
-    #     print(type(q))
-    #     '''
-    #     Generate q's successors and set their parents to q
-    #     # '''
+    for successor in successors:
+        if successor.curr_grid == goal_grid:
+            print("Goal reached")
+            break
+        else:
+            successor.g = q.g
 
 
 a_star_search(easy_grid, True)
-
-# while open_list :
-#     curr_
-#
-#
-#
-# def print_state(state):
-#     print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in state]))
-#
-#
-#
-# def main():
-#     start_state_str = input("Please enter the start state as a comma separated vector excluding '<' and '>'. "
-#                             "For blank type b:\n"
-#                             "Ex: <1,2,3,4,5,6,7,8,b>\n")
-#     goal_state_str = input("Please enter the goal state as a comma separated vector excluding '<' and '>'. "
-#                            "For blank type b:\n"
-#                            "Ex: <1,2,3,4,5,6,7,8,b>\n")
-#     # print(start_state_str)
-#     start_state = np.reshape(start_state_str.split(","), (3,3))
-#     goal_state = np.reshape(goal_state_str.split(","), (3,3))
-#     # print(start_state)
-#     # print_state(start_state, goal_state)
-#
-#
-# if __name__ == '__main__':
-#     main()
