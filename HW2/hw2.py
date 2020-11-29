96be7c842ff1f36af14b7793932ef723b1b06833
Yaniv Bronshtein <yanbronshtein@gmail.com>
2020-11-29 17:57:43 -0500

    Create theta_parameters function

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -3,12 +3,7 @@ import os
 import shutil
 
 # starting parameters
-theta_gender_is_M = 0.7
-theta_weight_gt_130_given_gender_is_M = 0.8
-theta_weight_gt_130_given_gender_isF = 0.4
-theta_height_gt_55_given_gender_is_M = 0.7
-theta_height_gt_5_given_gender_is_F = 0.3
-threshold = 0.0001
+
 
 """
 Hint: E-step of the EM algorithm is essentially estimating the probabilities of different 
@@ -47,10 +42,42 @@ def parse_data(filename):
         # data_dict[line] = 1 if line not in data_dict else data_dict[line] + 1  # Put each line as the key and the
         # value is the frequency
 
-
         # unique_vowels = {i for i in quote if i in 'aeiou'}
     return data_dict
 
 
+def e_step(data_dict, parameter_set):
+    for key in data_dict:
+        # estimate p(gender|weight=0,height=0)
+        if key == ('-', '0', '0'):
+            pass
 
-parse_data('hw2dataset_10.txt')
+        elif key == ('-', '0', '1'):
+            pass
+
+        elif key == ('-', '1', '0'):
+            pass
+        elif key == ('-', '1', '1'):
+            pass
+        else:
+            raise Exception("The data", key, "is not formatted correctly")
+
+
+def theta_parameters(gender_0, weight_0_given_gender_0, weight_0_given_gender_1, height_0_given_gender_0,
+                     height_0_given_gender1):
+    return {
+        ('0', 'd', 'd'): gender_0,
+        ('1', 'd', 'd'): 1 - gender_0, ('0', '0', 'd'): weight_0_given_gender_0,
+        ('0', '1', 'd'): 1 - weight_0_given_gender_0,
+        ('1', '0', 'd'): weight_0_given_gender_1,
+        ('1', '1', 'd'): 1 - weight_0_given_gender_1, ('0', 'd', '0'): height_0_given_gender_0,
+        ('0', 'd', '1'): 1 - height_0_given_gender_0,
+        ('1', 'd', '0'): height_0_given_gender1,
+        ('1', 'd', '1'): 1 - height_0_given_gender1
+    }
+
+
+data_dict = parse_data('hw2dataset_10.txt')
+theta = theta_parameters(gender_0=0.7, weight_0_given_gender_0=0.8,
+                         weight_0_given_gender_1=0.4, height_0_given_gender_0=0.7, height_0_given_gender1=0.3)
+e_step(data_dict=data_dict, parameter_set=theta)
